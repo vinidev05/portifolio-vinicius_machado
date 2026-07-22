9d8a86b35553c40c1942d9443d116d893aa972b3
vinidev05 <vini06041013@gmail.com>
2026-07-22 13:41:07 -0300

    feat: adiciona dados dos projetos

---
+++ src/data/projects.js
@@ -0,0 +1,107 @@
+const projects = [
+  {
+    id: 1,
+    title: "Portal Viagens",
+    description:
+      "Aplicação desenvolvida em Next.js com foco em otimização de desempenho utilizando Lighthouse, imagens WebP, lazy loading e boas práticas de SEO.",
+
+    technologies: [
+      "Next.js",
+      "React",
+      "TypeScript",
+      "Tailwind CSS"
+    ],
+
+    github: "https://github.com/vinidev05",
+
+    demo: "",
+
+    image: "/images/portal-viagens.png"
+  },
+
+  {
+    id: 2,
+    title: "Micro Frontends",
+
+    description:
+      "Projeto utilizando Module Federation para dividir a aplicação em micros independentes, integrando Cardápio, Pedido e Container.",
+
+    technologies: [
+      "React",
+      "Module Federation",
+      "JavaScript",
+      "Vite"
+    ],
+
+    github: "https://github.com/vinidev05",
+
+    demo: "",
+
+    image: "/images/microfrontends.png"
+  },
+
+  {
+    id: 3,
+
+    title: "Blog Next.js",
+
+    description:
+      "Blog desenvolvido em Next.js utilizando rotas dinâmicas, geração estática de páginas e otimizações para SEO.",
+
+    technologies: [
+      "Next.js",
+      "React",
+      "TypeScript"
+    ],
+
+    github: "https://github.com/vinidev05",
+
+    demo: "",
+
+    image: "/images/blog.png"
+  },
+
+  {
+    id: 4,
+
+    title: "Lista de Tarefas",
+
+    description:
+      "Aplicação para gerenciamento de tarefas desenvolvida em React utilizando componentes reutilizáveis.",
+
+    technologies: [
+      "React",
+      "JavaScript",
+      "Styled Components"
+    ],
+
+    github: "https://github.com/vinidev05",
+
+    demo: "",
+
+    image: "/images/todo.png"
+  },
+
+  {
+    id: 5,
+
+    title: "Catálogo de Produtos",
+
+    description:
+      "Projeto desenvolvido em React apresentando um catálogo de produtos organizado em componentes reutilizáveis.",
+
+    technologies: [
+      "React",
+      "Styled Components",
+      "JavaScript"
+    ],
+
+    github: "https://github.com/vinidev05",
+
+    demo: "",
+
+    image: "/images/catalogo.png"
+  }
+];
+
+export default projects;
